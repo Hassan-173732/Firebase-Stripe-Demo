8c5fe5a1f29034b39ada643aa4c224636640c202
Hassan Azhar <hassanazhar173732@gmail.com>
2024-01-15 11:09:12 +0100

    Update Firebase configuration

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB-L-nBSQa03D-tzNU9FvvgAx8VMbLG-4g",
-  authDomain: "stripe-subscriptions-2e77d.firebaseapp.com",
-  projectId: "stripe-subscriptions-2e77d",
-  storageBucket: "stripe-subscriptions-2e77d.appspot.com",
-  messagingSenderId: "465407925299",
-  appId: "1:465407925299:web:a96b4f9cec4b75abe95746"
+  apiKey:'', // YOUR API KEY
+  authDomain:'', // YOUR AUTH DOMAIN,
+  projectId:'', // YOUR PROJECT ID,",
+  storageBucket:'', // YOUR STORAGE BUCKET,
+  messagingSenderId:'', // YOUR MESSAGING SENDER ID,
+  appId: '' // YOUR APP ID,
 };
 
 // Initialize Firebase
